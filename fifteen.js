e205aac229583f17c2f557e1649c235e79477706
simonerscott <rhoniscott@yahoo.com>
2016-10-28 02:43:56 -0500

    Modified the for loop

--- fifteen.js
+++ fifteen.js
@@ -2,14 +2,13 @@ $(document).ready(function()
 {
 	var puzzleArea = $("puzzlearea");
 	var puzzlePieces = puzzleArea.children();
-	puzzleArea.style.backgroundImage = 'background.jpg';
+	//puzzleArea.style.backgroundImage = 'background.jpg';
 
 	console.log(puzzlePieces.length);
-	//puzzlePieces.style.backgroundImage("url('background.jpg')");
-
+	
 	for (var i = 0; i < puzzlePieces.length; i++)
 	{
-		
+		puzzlePieces.style.backgroundImage("url('background.jpg')");
 		puzzlePieces[i].addClass("puzzlepiece");
 		puzzlepiece.id = i;
 	}
