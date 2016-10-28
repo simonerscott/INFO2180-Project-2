50fc4c58f14977387f1fcd909095b5ca0b7d6591
simonerscott <rhoniscott@yahoo.com>
2016-10-28 08:24:58 -0500

    Added counter in the for loop

--- fifteen.js
+++ fifteen.js
@@ -8,9 +8,11 @@ $(document).ready(function()
 	
 	for (var i = 0; i < puzzlePieces.length; i++)
 	{
+		count = 1;
 		puzzlePieces.style.backgroundImage("url('background.jpg')");
 		puzzlePieces[i].addClass("puzzlepiece");
-		puzzlepiece.id = i;
+		puzzlepiece.id = count;
+		count++;
 	}
 
 
