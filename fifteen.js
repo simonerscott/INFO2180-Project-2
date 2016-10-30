e4f3316cbe812f6961579ee0abc44ec2e51db1bb
simonerscott <rhoniscott@yahoo.com>
2016-10-30 11:57:21 -0500

    Attemping to write move function: Unsuccessful

--- fifteen.js
+++ fifteen.js
@@ -1,8 +1,14 @@
 $(document).ready(function()
 {
 	var puzzlePieces = $("#puzzlearea div");
-	var counter = 0
+	var counter = 1;
+	var blankX = "300px";
+	var blankY = "300px";
+	var tempX = "";
+	var tempY = "";
+	var list = [];
 
+	
 	
 	//for loop to apply background image
 	for (var i = 0; i < puzzlePieces.length; i++)
@@ -11,15 +17,47 @@ $(document).ready(function()
 		puzzlePieces[i].className = "puzzlepiece";
 		puzzlePieces.id = counter;
 		setBackground(i, puzzlePieces);
+		list[i] = puzzlePieces.id; 
 		counter++;
 	}
+	list[15] = 0;
+	 
 
+			
 
 	// Function created to place puzzle pieces in the correct order
 	function setBackground(index, array)
-	{
+	{		
 		puzzlePieces[i].style.left = (i % 4 * 100) +'px';
 		puzzlePieces[i].style.top = (parseInt(i/4)*100) + 'px';
 		puzzlePieces[i].style.backgroundPosition = '-' + puzzlePieces[i].style.left + ' ' + '-' + puzzlePieces[i].style.top;
+    }
+
+    
+	
+
+	function shuffle()
+	{
+
 	}
+
+	
+
+	/*puzzlePieces.onclick = movePiece();
+	function movePiece()
+	{
+		console.log("Tester");
+		var id = puzzlePieces.id;
+		console.log(id);
+	}*/
+
+	puzzlePieces.click(function(event)
+	{
+		console.log("Tester");
+		//var id = event.currentTarget.id;
+		console.log(this.id);
+		
+	});
+		
+
 });
