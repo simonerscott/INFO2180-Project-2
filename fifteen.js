18ce38862df5b9a16ece0e2a0aad211f02205c29
simonerscott <rhoniscott@yahoo.com>
2016-10-30 20:58:44 -0500

    Able to move clicked puzzle piece to empty space

--- fifteen.js
+++ fifteen.js
@@ -2,13 +2,13 @@ $(document).ready(function()
 {
 	var puzzlePieces = $("#puzzlearea div");
 	var counter = 1;
-	var blankX = "300px";
-	var blankY = "300px";
+	var blankX = "300px"; //Left Coordinates for empty space
+	var blankY = "300px"; //Right Coordinates for empty space
 	var tempX = "";
 	var tempY = "";
 	var list = [];
 
-	
+		
 	
 	//for loop to apply background image
 	for (var i = 0; i < puzzlePieces.length; i++)
@@ -43,20 +43,14 @@ $(document).ready(function()
 
 	
 
-	/*puzzlePieces.onclick = movePiece();
-	function movePiece()
+	puzzlePieces.click(function()
 	{
-		console.log("Tester");
-		var id = puzzlePieces.id;
-		console.log(id);
-	}*/
-
-	puzzlePieces.click(function(event)
-	{
-		console.log("Tester");
-		//var id = event.currentTarget.id;
-		console.log(this.id);
-		
+		x =  $(this).css("left");
+		y =  $(this).css("top");
+		$(this).css("left", blankX);
+		$(this).css("top", blankY);
+		blankX = x;
+		blankY = y;
 	});
 		
 
