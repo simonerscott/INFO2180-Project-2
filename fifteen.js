790e2cfd0ffc68d3cbbcb1ff127aba46e2271cc2
simonerscott <rhoniscott@yahoo.com>
2016-11-01 18:13:06 -0500

    At present the validMove fucntion is returning the correctvalue

--- fifteen.js
+++ fifteen.js
@@ -8,19 +8,30 @@ $(document).ready(function()
 	var tempY = "";
 	// var list = [];
 	var nList = new Object();
+	var found = false;
 
-		
 	
+	
+	function selectPicture()
+	{
+		var button = $("#control");
+		var pictureChoice = prompt("");
+	}
+
+	
+
 	//for loop to apply background image
 	for (var i = 0; i < puzzlePieces.length; i++)
 	{
 		puzzlePieces[i].style.backgroundImage = "url('background.jpg')";
 		puzzlePieces[i].className = "puzzlepiece";
 		puzzlePieces.id = counter;
+		$(puzzlePieces[i]).attr("id",counter);
 		setBackground(i, puzzlePieces);
 		// list[i] = puzzlePieces.id; 
-		// counter++;
+		 counter++;
 	}
+	$(puzzlePieces[15]).attr("id", 0);
 	//list[15] = 0;
 	
 	 
@@ -31,7 +42,7 @@ $(document).ready(function()
 	function setBackground(index, array)
 	{		
 		puzzlePieces[i].style.left = (i % 4 * 100) +'px';
-		puzzlePieces[i].style.top = (parseInt(i/4)*100) + 'px';
+		puzzlePieces[i].style.top = (parseInt(i/4)*100) + 'px'; // recalculate y coordinate it is assuming the value the y 
 		puzzlePieces[i].style.backgroundPosition = '-' + puzzlePieces[i].style.left + ' ' + '-' + puzzlePieces[i].style.top;
     }
 
@@ -48,38 +59,23 @@ $(document).ready(function()
 	//
 	puzzlePieces.click(function()
 	{			
-		x =  $(this).css("left");
-		y =  $(this).css("top");
+		var x =  $(this).css("left");
+		var y =  $(this).css("top");
 		$(this).css("left", blankX); // setting the offset().left to equal empty space x-coordinates
 		$(this).css("top", blankY); // setting the offset().top to equal empty space y-coordinates
 		blankX = x;
 		blankY = y;
 		//puzzlePieces.hover = validMove(); //idk if being called correctly
-		//console.log(nList);
 	});
 	
 
 	
 
-	puzzlePieces.mouseover(validMove());
-	/*puzzlePieces.forEach(function(item)
-	{
-		item.mouseover(function)
-		{
-			if (validMove() == true)
-			{
-				$(this).addClassName("movablepiece");
-			}
-			else
-			{
-				$(this).removeClassName("movablepiece");
-			}
-		});
-	});
-*/
+	puzzlePieces.mouseover(validMove);
 
 	function validMove()
 	{
+
 		var lessThanZero = 0;
 		var greaterThan300 = 300;
 		var topNeighbor;
@@ -88,22 +84,16 @@ $(document).ready(function()
 		var rightNeighbor;
 		var neighborList;
 
-		/*var x =  $((this).css("left"));
+		var x =  $(this).css("left");
 		var y =  $(this).css("top");
 		x = parseInt(x);
-		y = parseInt(y);*/
-		//console.log("X: " + x + " Y: " + y);
+		y = parseInt(y);
 		blankY = parseInt(blankY);
 		blankX = parseInt(blankX);
 
 
-
 		//Empty the list before other neighbor coordinates are added
-		x = nList.length;
-		for (i = 0; i < x; i++) 
-		{
-			nList.pop();
-		}
+		nList = [];
 
 
 		topNeighbor = blankY - 100;
@@ -123,7 +113,7 @@ $(document).ready(function()
 			//nList.push(bottonNeighbor);
 		}
 
-		leftNeighbor = blankX - 100;
+		leftNeighbor = parseInt(blankX) - 100;
 		if (leftNeighbor >= lessThanZero)
 		{
 			nList["left"] = [leftNeighbor, blankY]
@@ -140,27 +130,28 @@ $(document).ready(function()
 		}
 
 
-		var found = false;
 		for (var key in nList)
 		{
 			var neighbor = nList[key];			
 			if (neighbor[0] == x && neighbor[1] == y)
 			{
-				console.log("X Value : " + neighbor[0] + " blankX Value:" + x);
-				console.log("Y Value : " + neighbor[1] + " blankY Value:" + y);
+				/*console.log("X Value : " + neighbor[0] + " blankX Value:" + x);
+				console.log("Y Value : " + neighbor[1] + " blankY Value:" + y);*/
+				console.log(found);
 				found = true;
 				return found;
 			}
-			console.log("X Value : " + neighbor[0] + " blankX Value:");
-			console.log("Y Value : " + neighbor[1] + " blankY Value:");	
-		}
+			/*console.log(nList);
+			console.log("X Value : " + neighbor[0]);
+			console.log("Y Value : " + neighbor[1]);*/
 
-		console.log(nList);
+		}
+		
+
 
 	}
+	
 
-	
-	
 		
 
 });
